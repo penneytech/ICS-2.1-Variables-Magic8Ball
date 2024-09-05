b14fc86cc1493b6c359958a573b1f4b66e18979d
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-05 19:45:13 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+/* 
+
+You are coding a magic 8 ball app that changes a random message when the user presses a button. 
+Imagine that the he app has a button at the bottom of it that the user presses, 
+and a text field above the button where that message appears.
+ 
+Declare a variable under each comment below stating:
+
+1) The appropriate type (let or const)
+2) An appropriate name
+3) An initial value of your choosing 
+
+*/
+
+// Five messages to use in the 8 ball
+
+// A placeholder variable for the current displayed message (do not assign this variable a value);
+
+// The number of times the button was pressed.
+
+// The text on the button. 
+ 
+// Set the placeholder variable (you previously declared) to equal one of the messages.
+
+// Log the message to the console. 
