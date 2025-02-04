575d785f0ab51c70576d96c1e9dca76a672cc2e9
penneytech <penneyp@hdsb.ca>
2025-02-04 09:47:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /* 
 
 You are coding a magic 8 ball app that changes a random message when the user presses a button. 
-Imagine that the he app has a button at the bottom of it that the user presses, 
+Imagine that the app has a button at the bottom of it that the user presses, 
 and a text field above the button where that message appears.
  
-Declare a variable under each comment below stating:
+Declare variables under each comment below stating:
 
 1) The appropriate type (let or const)
 2) An appropriate name
@@ -13,13 +13,19 @@ Declare a variable under each comment below stating:
 */
 
 // Five messages to use in the 8 ball
+const messageOne = "Yes, it is certain.";
+    // ... four more messages here
+
 
 // A placeholder variable for the current displayed message (do not assign this variable a value);
-
-// The number of times the button was pressed.
-
-// The text on the button. 
  
-// Set the placeholder variable (you previously declared) to equal one of the messages.
 
-// Log the message to the console. 
+// Set the placeholder variable above to equal one of the five messages you created.
+
+
+// Use console.log() to print the placeholder variable to the console. 
+
+
+// Declare a variable to track the number of times the button was pressed, and assign it a dummy value.
+
+
